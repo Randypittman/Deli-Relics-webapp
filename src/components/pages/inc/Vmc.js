0d4fb149fd52c5d28a36443b1077f9881779bc35
Randypittman <158580250+Randypittman@users.noreply.github.com>
2024-04-06 18:01:29 +0000

    Add files via upload

---
+++ src/components/pages/inc/Vmc.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+function Vmc() {
+  return(
+<section className="section bg-c-light border-top">
+  <div className="container">
+    <div className="row">
+      <div className="col-md-12 mb-5 text-center">
+      <h3 className="main-heading">Vision,Mission and Values</h3>
+      <div className="underline mx-auto"></div>
+      </div>
+
+      <div className="col-md-4">
+        <h6>Our Vision</h6>
+        <p>
+        Our Vision is to grant Job OPPORTUNITIES for our supported bussines investors that spends much time on my our programs thats comes up.This folks are Classic MECHANICS,SPAYERS,PARTS DEALERS AND ETC.
+        </p>
+      </div>
+
+      <div className="col-md-4 ">
+      <h6>Our Mission</h6>
+        <p>
+          Our Mission is to grant Job OPPORTUNITIES for bussines investorswhom  are Classic MECHANICS,SPAYERS,PARTS DEALERS AND ETC.
+        </p>
+      </div>
+
+      <div className="col-md-4">
+        <h6>Our Core Values</h6>
+        <p>
+          Our Values is to grant Job OPPORTUNITIES for our supported bussines investors that spends much time on my our programs thats comes up with a chance of engaging in a raffle exercise and win or claiming some prices for themselves        </p>
+      </div>
+
+      </div>
+
+    </div>
+
+</section>
+  );
+}
+export default Vmc;
